69d4ea88571bed948c5d5c0a749f67aca828b742
Florian Mai <florian.ren.mai@googlemail.com>
2013-10-26 14:45:18 -0700

    [NETWORKING][EVENTINGSERVER][TEST] Added interactivity to chat clients for testing.

--- src/de/eduras/eventingserver/test/ChatClient.java
+++ src/de/eduras/eventingserver/test/ChatClient.java
@@ -1,5 +1,9 @@
 package de.eduras.eventingserver.test;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 import de.eduras.eventingserver.Client;
 import de.eduras.eventingserver.ClientInterface;
 import de.eduras.eventingserver.Event;
@@ -9,10 +13,38 @@ public class ChatClient {
 		ClientInterface client = new Client();
 		client.setEventHandler(new ChatEventHandlerClient(client));
 		client.connect("localhost", 6666);
-		String chatMessage = "Sample Chat message!";
-		Event chatEvent = new Event(ChatEventHandlerServer.MESSAGE_SENT_EVENT);
-		chatEvent.putArgument("Jannis");
-		chatEvent.putArgument(chatMessage);
-		client.sendEvent(chatEvent);
+
+		BufferedReader userInputReader = new BufferedReader(
+				new InputStreamReader(System.in));
+
+		System.out.println("Hi! What's your name, Sir?");
+		String name;
+		try {
+			name = userInputReader.readLine();
+		} catch (IOException e1) {
+			e1.printStackTrace();
+			return;
+		}
+
+		boolean running = true;
+		while (running) {
+
+			String userInput;
+			try {
+				userInput = userInputReader.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+				continue;
+			}
+			if (userInput.equals("/quit")) {
+				running = false;
+			} else {
+				Event messageEvent = new Event(
+						ChatEventHandlerServer.MESSAGE_SENT_EVENT);
+				messageEvent.putArgument(name);
+				messageEvent.putArgument(userInput);
+				client.sendEvent(messageEvent);
+			}
+		}
 	}
 }
